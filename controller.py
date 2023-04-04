fc5dcd650d375b32688c253db35e71093fcff579
Sergey <smtishkar@mail.ru>
2023-04-04 21:29:53 +0300

    initial commit

---
+++ controller.py
@@ -0,0 +1,6 @@
+import ui
+
+
+def run():
+    user_request = ui.choose_action()
+    
---
+++ ui.py
@@ -0,0 +1,25 @@
+import uuid
+
+def choose_action():
+    print (f"Выберите действие: \nСоздать заметку - 1\nСписок заметок - 2\n"
+                "Редактировать заметку - 3\nУдалить заметку - 4\n")
+    request = int(input("Введите значение: "))
+    return request
+
+
+def create():
+    id
+    title
+    topic
+    date
+    
+    pass
+
+def see_all():
+    pass
+
+def correct():
+    pass
+
+def delete():
+    pass
